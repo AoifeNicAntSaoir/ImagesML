af928c3a860afffd445acd3019cf086e084a4ced
Aoife Sayers <aoifesayers@gmail.com>
2017-03-16 08:39:21 +0000

    Improved Image Processing

--- ImgRecognition2/src/mse&SSIM.py
+++ ImgRecognition2/src/mse&SSIM.py
@@ -1,18 +1,21 @@
 from PIL import Image
 from sklearn.metrics import mean_squared_error
-from skimage.measure import structural_similarity, compare_ssim
+from skimage.measure import compare_ssim
 import numpy as np
 
-
-
 ima3 = np.array(Image.open("2ii.png").convert('L'))
-img3= Image.open("2ii.png")
-print img3.size[0]
-if img3.size[0]>8:
-    out = img3.thumbnail((8,8),Image.ANTIALIAS)
-    #print img3.size[0]
 ima4 = np.array(Image.open("2i.png").convert('L'))
-
 print mean_squared_error(ima3, ima4)
-
 print compare_ssim(ima3, ima4)
+
+
+ima5 = Image.open("2ii.png").resize((8,8)).convert('L')
+image5 = np.array(ima5)
+print image5
+
+ima6 = Image.open("2.png").resize((8,8)).convert('L')
+image6 = np.array(ima6)
+print image6
+
+print compare_ssim(image5, image6)
+print mean_squared_error(image5, image6)
